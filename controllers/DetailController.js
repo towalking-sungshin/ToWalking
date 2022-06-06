415d8d60d2dd9df578639ec9a6b78ece06a0d822
zinzo1019 <topjoy22@naver.com>
2022-06-07 00:06:54 +0900

    [DetailController.js] ejs 파일에 사용자 아이디를 넘겨주도록 수정

--- controllers/DetailController.js
+++ controllers/DetailController.js
@@ -1,6 +1,6 @@
-const db = require(__dirname + "/../models/yujin_index")
+const db = require(__dirname + "/../models/index")
 const tw_Trail = db.tw_Trail;
-const user_Trail = db.user_Trail;
+const user_tw_table = db.user_tw_table;
 const review = db.review;
 
 /** 산책로 리스트 가져오기 */
@@ -29,7 +29,7 @@ exports.getTWTrailDetails = async (req, res) => {
             }
         });
         console.log(reviewList);
-        res.render("../views/tw_TrailDetail", {trailDetail: data, reviews: reviewList});
+        res.render("../views/tw_TrailDetail", {user_id: req.params.user_id, trailDetail: data, reviews: reviewList});
     } catch (err) {
         res.status(500).send({
             message: err.message
@@ -40,7 +40,7 @@ exports.getTWTrailDetails = async (req, res) => {
 /** 특정 사용자 산책로 상세 정보 가져오기 */
 exports.getUSERTrailDetails = async (req, res) => {
     try {
-        data = await user_Trail.findByPk(req.params.id);
+        data = await user_tw_table.findByPk(req.params.id);
         console.log(data);
 
         reviewList = await review.findAll({
